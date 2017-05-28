e76abde2796f6d905a2ab8d6d4fc95c6129d7e57
Raoul v. R <vanruesc@outlook.de>
2017-05-28 19:37:19 +0200

    Added a test for SymmetricMatrix3.

--- test/math.3d.test.js
+++ test/math.3d.test.js
@@ -39,6 +39,47 @@ module.exports = {
 			test.ok(sm);
 			test.done();
 
+		},
+
+		"correctly transforms vectors": function(test) {
+
+			const m0 = new lib.SymmetricMatrix3();
+			const m1 = new lib.Matrix3();
+
+			const v0 = new lib.Vector3(0, 1, 2);
+			const v1 = v0.clone();
+			const v2 = v0.clone();
+			const v3 = v0.clone();
+
+			m0.identity();
+			m1.identity();
+
+			m0.applyToVector3(v0);
+			v1.applyMatrix3(m1);
+
+			m0.set(
+
+				1, 2, 3,
+				2, 3,
+				3
+
+			);
+
+			m1.set(
+
+				1, 2, 3,
+				2, 2, 3,
+				3, 3, 3
+
+			);
+
+			m0.applyToVector3(v2);
+			v3.applyMatrix3(m1);
+
+			test.ok(v0.equals(v1), "should compute the same result as its complete matrix equivalent");
+			test.ok(v2.equals(v3), "should compute the same result as its complete matrix equivalent");
+			test.done();
+
 		}
 
 	},
