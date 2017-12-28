c32b5fb6502ceb85bafba8a14f9039dc74385c04
Raoul v. R <vanruesc@outlook.de>
2017-12-28 21:23:06 +0100

    Use Number type internally.

--- src/RotationOrder.js
+++ src/RotationOrder.js
@@ -2,21 +2,21 @@
  * An enumeration of Euler rotation orders.
  *
  * @type {Object}
- * @property {String} XYZ - X -> Y -> Z.
- * @property {String} YZX - Y -> Z -> X.
- * @property {String} ZXY - Z -> X -> Y.
- * @property {String} XZY - X -> Z -> Y.
- * @property {String} YXZ - Y -> X -> Z.
- * @property {String} ZYX - Z -> Y -> X.
+ * @property {Number} XYZ - X -> Y -> Z.
+ * @property {Number} YZX - Y -> Z -> X.
+ * @property {Number} ZXY - Z -> X -> Y.
+ * @property {Number} XZY - X -> Z -> Y.
+ * @property {Number} YXZ - Y -> X -> Z.
+ * @property {Number} ZYX - Z -> Y -> X.
  */
 
 export const RotationOrder = {
 
-	XYZ: "XYZ",
-	YZX: "YZX",
-	ZXY: "ZXY",
-	XZY: "XZY",
-	YXZ: "YXZ",
-	ZYX: "ZYX"
+	XYZ: 0,
+	YZX: 1,
+	ZXY: 2,
+	XZY: 3,
+	YXZ: 4,
+	ZYX: 5
 
 };
