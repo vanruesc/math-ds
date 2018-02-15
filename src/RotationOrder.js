50195921cf2a09a53ccede408373d8f7d314d369
Raoul v. R <vanruesc@outlook.de>
2018-02-15 14:29:56 +0100

    Revert "Use Number type internally."
    This reverts commit c32b5fb6502ceb85bafba8a14f9039dc74385c04.

--- src/RotationOrder.js
+++ src/RotationOrder.js
@@ -2,21 +2,21 @@
  * An enumeration of Euler rotation orders.
  *
  * @type {Object}
- * @property {Number} XYZ - X -> Y -> Z.
- * @property {Number} YZX - Y -> Z -> X.
- * @property {Number} ZXY - Z -> X -> Y.
- * @property {Number} XZY - X -> Z -> Y.
- * @property {Number} YXZ - Y -> X -> Z.
- * @property {Number} ZYX - Z -> Y -> X.
+ * @property {String} XYZ - X -> Y -> Z.
+ * @property {String} YZX - Y -> Z -> X.
+ * @property {String} ZXY - Z -> X -> Y.
+ * @property {String} XZY - X -> Z -> Y.
+ * @property {String} YXZ - Y -> X -> Z.
+ * @property {String} ZYX - Z -> Y -> X.
  */
 
 export const RotationOrder = {
 
-	XYZ: 0,
-	YZX: 1,
-	ZXY: 2,
-	XZY: 3,
-	YXZ: 4,
-	ZYX: 5
+	XYZ: "XYZ",
+	YZX: "YZX",
+	ZXY: "ZXY",
+	XZY: "XZY",
+	YXZ: "YXZ",
+	ZYX: "ZYX"
 
 };
