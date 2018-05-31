d8d3fcf29dfc2717eb2e2416591611377375c5de
Raoul v. R <vanruesc@outlook.de>
2018-05-31 14:23:01 +0200

    Optimized makeRotationFromQuaternion(q).

--- src/Matrix4.js
+++ src/Matrix4.js
@@ -508,38 +508,7 @@ export class Matrix4 {
 
 	makeRotationFromQuaternion(q) {
 
-		const te = this.elements;
-
-		const x = q.x, y = q.y, z = q.z, w = q.w;
-		const x2 = x + x, y2 = y + y, z2 = z + z;
-		const xx = x * x2, xy = x * y2, xz = x * z2;
-		const yy = y * y2, yz = y * z2, zz = z * z2;
-		const wx = w * x2, wy = w * y2, wz = w * z2;
-
-		te[0] = 1 - (yy + zz);
-		te[4] = xy - wz;
-		te[8] = xz + wy;
-
-		te[1] = xy + wz;
-		te[5] = 1 - (xx + zz);
-		te[9] = yz - wx;
-
-		te[2] = xz - wy;
-		te[6] = yz + wx;
-		te[10] = 1 - (xx + yy);
-
-		// Last column.
-		te[3] = 0;
-		te[7] = 0;
-		te[11] = 0;
-
-		// Bottom row.
-		te[12] = 0;
-		te[13] = 0;
-		te[14] = 0;
-		te[15] = 1;
-
-		return this;
+		return this.compose(a.set(0, 0, 0), q, b.set(1, 1, 1));
 
 	}
 
