21092428b9b90ff5981707941cddb48a0b46fe3b
Raoul v. R <vanruesc@outlook.de>
2018-05-31 14:22:27 +0200

    Optimized compose(position, quaternion, scale).

--- src/Matrix4.js
+++ src/Matrix4.js
@@ -1057,9 +1057,35 @@ export class Matrix4 {
 
 	compose(position, quaternion, scale) {
 
-		this.makeRotationFromQuaternion(quaternion);
-		this.scale(scale.x, scale.y, scale.z);
-		this.setPosition(position);
+		const te = this.elements;
+
+		const x = quaternion.x, y = quaternion.y, z = quaternion.z, w = quaternion.w;
+		const x2 = x + x,	y2 = y + y, z2 = z + z;
+		const xx = x * x2, xy = x * y2, xz = x * z2;
+		const yy = y * y2, yz = y * z2, zz = z * z2;
+		const wx = w * x2, wy = w * y2, wz = w * z2;
+
+		const sx = scale.x, sy = scale.y, sz = scale.z;
+
+		te[0] = (1 - (yy + zz)) * sx;
+		te[1] = (xy + wz) * sx;
+		te[2] = (xz - wy) * sx;
+		te[3] = 0;
+
+		te[4] = (xy - wz) * sy;
+		te[5] = (1 - (xx + zz)) * sy;
+		te[6] = (yz + wx) * sy;
+		te[7] = 0;
+
+		te[8] = (xz + wy) * sz;
+		te[9] = (yz - wx) * sz;
+		te[10] = (1 - (xx + yy)) * sz;
+		te[11] = 0;
+
+		te[12] = position.x;
+		te[13] = position.y;
+		te[14] = position.z;
+		te[15] = 1;
 
 		return this;
 
