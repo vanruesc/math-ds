ac14fe80759fe7c6e0805d364d40591f964a81cc
Raoul v. R <vanruesc@users.noreply.github.com>
2020-01-23 15:37:24 +0100

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,68 +1,59 @@
 import babel from "rollup-plugin-babel";
-import minify from "rollup-plugin-babel-minify";
-import resolve from "rollup-plugin-node-resolve";
+import resolve from "@rollup/plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
 
 const pkg = require("./package.json");
 const date = (new Date()).toDateString();
 
+const production = (process.env.NODE_ENV === "production");
+
 const banner = `/**
  * ${pkg.name} v${pkg.version} build ${date}
  * ${pkg.homepage}
- * Copyright ${date.slice(-4)} ${pkg.author.name}, ${pkg.license}
+ * Copyright ${date.slice(-4)} ${pkg.author.name}
+ * @license ${pkg.license}
  */`;
-
-const production = (process.env.NODE_ENV === "production");
 
 const lib = {
 
-	esm: {
-
+	module: {
 		input: "src/index.js",
 		plugins: [resolve()],
 		output: [{
+			file: pkg.module,
+			format: "esm",
+			banner
+		}, {
 			file: pkg.main,
 			format: "esm"
-		}].concat(!production ? [] : [
-			{
-				file: pkg.module,
-				format: "esm",
-				banner: banner
-			}, {
-				file: pkg.main.replace(".js", ".min.js"),
-				format: "esm"
-			}
-		])
-
+		}, {
+			file: pkg.main.replace(".js", ".min.js"),
+			format: "esm"
+		}]
 	},
 
-	umd: {
-
-		input: pkg.main,
+	main: {
+		input: production ? pkg.main : "src/index.js",
 		plugins: production ? [babel()] : [],
 		output: {
 			file: pkg.main,
 			format: "umd",
 			name: pkg.name.replace(/-/g, "").toUpperCase(),
-			banner: banner
+			banner
 		}
+	},
 
+	min: {
+		input: pkg.main.replace(".js", ".min.js"),
+		plugins: [terser(), babel()],
+		output: {
+			file: pkg.main.replace(".js", ".min.js"),
+			format: "umd",
+			name: pkg.name.replace(/-/g, "").toUpperCase(),
+			banner
+		}
 	}
 
 };
 
-export default [lib.esm, lib.umd].concat(production ? [{
-
-	input: lib.esm.output[2].file,
-	plugins: [babel(), minify({
-		bannerNewLine: true,
-		comments: false
-	})],
-	output: {
-		file: lib.esm.output[2].file,
-		format: "umd",
-		name: pkg.name.replace(/-/g, "").toUpperCase(),
-		banner: banner
-	}
-
-}] : []);
-
+export default production ? [lib.module, lib.main, lib.min] : [lib.main];
